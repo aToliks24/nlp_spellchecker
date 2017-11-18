4e487284aae42cad8515912172fa0743b356fbcc
tolik <shustera@post.bgu.ac.il>
2017-11-18 20:54:50 +0200

    mistakesCountDict

--- spell_checker.py
+++ spell_checker.py
@@ -1,15 +1,51 @@
 import re
 import time
 from enum import Enum
-class MistakeType(Enum):
-    Deletion=0
-    Insertion=1
-    Substitution=2
-    Transposition=3
 
-MAX_MISTAKES_IN_WORD=5
+Deletion='deletion'
+Insertion='insertion'
+Substitution='substitution'
+Transposition='transposition'
+
+mistakesProbDict = {
+    Insertion: dict(),
+    Deletion: dict(),
+    Substitution: dict(),
+    Transposition: dict()
+}
+mistakesCountDict = {
+    Insertion: dict(),
+    Deletion: dict(),
+    Substitution: dict(),
+    Transposition: dict()
+}
+
+charactersCountDict = dict()
+
+MAX_MISTAKES_IN_WORD=3
 
 
+def delition(cp_m_1, cp):
+    if (cp_m_1 , cp) not in mistakesCountDict[Deletion] or cp_m_1 + cp not in charactersCountDict:
+        return 0
+    return (mistakesCountDict[Deletion][(cp_m_1 , cp)]) / (charactersCountDict[cp_m_1 + cp])
+def insertion(cp_m_1, tp):
+    if (cp_m_1 , tp) not in mistakesCountDict[Deletion] or cp_m_1 not in charactersCountDict:
+        return 0
+    return mistakesCountDict[Insertion][(cp_m_1 , tp)] / charactersCountDict[cp_m_1]
+def substitution(tp,cp):
+    if (tp , cp) not in mistakesCountDict[Deletion] or cp not in charactersCountDict:
+        return 0
+    return mistakesCountDict[Substitution][(tp , cp)] / charactersCountDict[cp]
+def transposition(cp,cp_p_1):
+    if (cp , cp_p_1) not in mistakesCountDict[Deletion] or cp + cp_p_1 not in charactersCountDict:
+        return 0
+    return mistakesCountDict[Transposition][(cp,cp_p_1)] / charactersCountDict[cp+cp_p_1]
+
+funcDict={Insertion:insertion,Deletion:delition,Substitution:substitution,Transposition:transposition}
+
+def calculateProbability(type,a,b):
+    return funcDict[type](a,b)
 
 def linsplitter(files):
     rgx1 = r'(?<=\w)[\x2e\x2d](?=\w)'  #U.S.A | key-word
@@ -26,6 +62,7 @@ def linsplitter(files):
                 res.append(r.lower())
     return res
 
+
 def ngrams(words, n):
   res = []
   grm=words[0]
@@ -36,19 +73,18 @@ def ngrams(words, n):
     res.append(words[i:i + n])
   return res
 
+
 def addToDict(ngramdict, grm):
     lst=grm.split(' ')
     k=lst[-1]
     v=lst[0:-1]
     if k in ngramdict:
-        if v in ngramdict[k]:
-            ngramdict[k][v]+=1
-        else:
-            ngramdict[k][v]=1
+        insertToCountDict(v,ngramdict[k])
     else:
         ngramdict[k]=dict()
         ngramdict[k][v]=1
     return ngramdict
+
 
 def learn_language_model(files, n=3, lm=None):
     """ Returns a nested dictionary of the language model based on the
@@ -96,41 +132,44 @@ def learn_language_model(files, n=3, lm=None):
 
 
 def getMistake(tup):
-    mis=tup[0]
-    tru=tup[1]
-    lst=recGetMistake(mis,tru,0)#list[tup(errTypeList, errTupList)*]
+    mis='^'+tup[0]
+    tru='^'+tup[1]
+    lst=recGetMistake(mis,1,tru,1,0)#list[tup(errTypeList, errTupList)*]
     return lst
 
-def recGetMistake(mis,tru,i):
+
+def recGetMistake(misake,mi,word,wi,i):
+    mis=misake[mi:]
+    tru=word[wi:]
     if i>MAX_MISTAKES_IN_WORD :
-        return []##########NONE
+        return []
     elif len(mis)==len(tru)==0:
         return []
     elif mis=="":
         if i+len(tru)>MAX_MISTAKES_IN_WORD:
-            return []##########NONE
+            return []
         else:
-            res=[]
-            for c in tru:
-                res.append((MistakeType.Deletion,(c,'-')))
+            res=[(Deletion,(word[wi-1],tru[0]))]
+            for c in tru[1:]:
+                res.append((Deletion,(c,'$')))
             return res
     elif tru=="":
         if i + len(mis) > MAX_MISTAKES_IN_WORD:
-            return []##########NONE
+            return []
         else:
-            res = []
-            for c in mis:
-                res.append((MistakeType.Insertion, ('-', c)))
+            res = [(Insertion,(misake[-1],mis[0]))]
+            for c in mis[1:]:
+                res.append((Insertion, ('$', c)))
             return res
     elif mis[0]==tru[0]:
-            return recGetMistake(mis[1:],tru[1:],i)
+            return recGetMistake(misake,mi+1,word,wi+1,i)
     else: #check errorType
-        subslist=[(MistakeType.Substitution,(tru[0],mis[0]))]+recGetMistake(mis[1:],tru[1:],i+1)
-        inserlist=[(MistakeType.Insertion,('-',mis[0]))]+recGetMistake(mis[1:],tru,i+1)
-        dellist=[(MistakeType.Deletion,(tru[0],'-'))]+recGetMistake(mis,tru[1:],i+1)
+        subslist=[(Substitution,(tru[0],mis[0]))]+recGetMistake(misake,mi+1,word,wi+1,i+1)
+        inserlist=[(Insertion,(word[wi-1],mis[0]))]+recGetMistake(misake,mi+1,word,wi,i+1)
+        dellist=[(Deletion,(word[wi-1],mis[0]))]+recGetMistake(misake,mi,word,wi+1,i+1)
         alllist = [subslist, inserlist, dellist]
         if len(tru)>1 and len(mis)>1 and tru[0]==mis[1] and tru[1]==mis[0]:
-            translist=[(MistakeType.Transposition,(tru[0:2],mis[0:2]))]+recGetMistake(mis[2:],tru[2:],i+1)
+            translist=[(Transposition,(tru[0],mis[0]))]+recGetMistake(misake,mi+2,word,wi+2,i+1)
             alllist.append(translist)
         res=None
         for i in range(len(alllist)):
@@ -143,6 +182,7 @@ def recGetMistake(mis,tru,i):
         if res==None:
             res=[]
         return res
+
 
 def create_error_distribution(errors_file, lexicon):
     """ Returns a dictionary {str:dict} where str is in:
@@ -164,47 +204,75 @@ def create_error_distribution(errors_file, lexicon):
         A dictionary of error distributions by error type (dict).
 
     """
-    mistakesCountDict={
-    MistakeType.Insertion:dict(),
-    MistakeType.Deletion:dict(),
-    MistakeType.Substitution:dict(),
-    MistakeType.Transposition:dict()
-    }
 
-    errCount=0
+
     f=open(errors_file,'r')
     wholeFile=f.read().lower().split('\n')
     f.close()
-    separated=[]
+    separated = errFileToTupleList(wholeFile)
+
+    for tup in separated:
+        errTups=getMistake(tup)
+        for curr in errTups:
+            errTup=curr[1]
+            errType=curr[0]
+            insertToCountDict(errTup,mistakesCountDict[errType])
+    for word in lexicon.keys():
+        for c in word:
+            insertToCountDict(c, charactersCountDict)
+        for i in range(len(word)-1):
+            c=word[i]+word[i+1]
+            insertToCountDict(c,charactersCountDict)
+    global mistakesProbDict
+    for etype in mistakesCountDict.keys():
+        for tup in mistakesCountDict[etype]:
+            p=calculateProbability(etype,tup[0],tup[1])
+            mistakesProbDict[etype][tup]=p
+
+
+def errFileToTupleList(wholeFile):
+    separated = []
     for str in wholeFile:
         if '->' not in str:
             continue
         tup = str.split('->')
         if ',' in tup[1]:
-            multiple=tup[1].split(',')
+            multiple = tup[1].split(',')
             for s in multiple:
-                separated.append((tup[0].strip(),s.strip()))
+                separated.append((tup[0].strip(), s.strip()))
         else:
-            separated.append((tup[0].strip(),tup[1].strip()))
-
-    for tup in separated:
-        errTups=getMistake(tup)
-        for curr in errTups:
-            errCount+=1
-            errTup=curr[1]
-            errType=curr[0]
-            if errTup in mistakesCountDict[errType].keys():
-                mistakesCountDict[errType][errTup]=mistakesCountDict[errType][errTup]+1
-            else:
-                mistakesCountDict[errType][errTup]=1
+            separated.append((tup[0].strip(), tup[1].strip()))
+    return separated
 
 
+def insertToCountDict(key, charactersCountDict):
+    if key in charactersCountDict:
+        charactersCountDict[key] += 1
+    else:
+        charactersCountDict[key] = 1
+
+
+def getwords(lex):
+    f=open(lex,'r')
+    str=f.read().lower()
+    filtered=re.findall('[a-z\x27\x2d]+',str)
+    words=dict()
+    for word in filtered:
+        insertToCountDict(word,words)
+    return words
 
 
 t1 = time.time()
 print(t1)
 path=r'C:\Users\tolik\Desktop\wikipedia_common_misspellings.txt'
-create_error_distribution(path,None)
+lex=r'C:\Users\tolik\Desktop\sharlock.txt'
+
+
+
+
+
+lexicon=getwords(lex)
+create_error_distribution(path,lexicon)
 t2 = time.time()
 print(str(t2)+'\n')
 t2=t2-t1
@@ -212,7 +280,7 @@ print(t2)
 
 
 
-'''''
+
 def generate_text(lm, m=15, w=None):
     """ Returns a text of the specified length, generated according to the
      specified language model using the specified word (if given) as an anchor.
@@ -224,13 +292,14 @@ def generate_text(lm, m=15, w=None):
 
     Returns:
         A sequrnce of generated tokens, separated by white spaces (str)
+
+    lm:=language model, m:=length of generated text
+    evaluate_text(s,lm)   s=sentence, lm language model
     """
 
 
-lm:=language model, m:=length of generated text
-evaluate_text(s,lm)   s=sentence, lm language model
 
-
+'''''
 def correct_word(w, word_counts, errors_dist):
     """ Returns the most probable correction for the specified word, given the specified prior error distribution.
 
@@ -266,7 +335,7 @@ Returns:
     The most probable sentence (str)
 
 """
-
+'''
 def evaluate_text(s,lm):
     """ Returns the likelihood of the specified sentence to be generated by the
     the specified language model.
@@ -278,4 +347,3 @@ def evaluate_text(s,lm):
     Returns:
         The likelihood of the sentence according to the language model (float).
     """
-'''
